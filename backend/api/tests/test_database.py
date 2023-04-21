031023eec200e6e2927c23917ecaaac9107093d0
Mustafa Gezen <mustafa@gezen.no>
2023-04-21 10:51:18 +0200

    re-enable database tests

--- backend/api/tests/test_database.py
+++ backend/api/tests/test_database.py
@@ -17,76 +17,77 @@ def client():
         yield test_client
 
 
-# @pytest.mark.asyncio
-# async def test_cleanup_db_data(client, monkeypatch):
+@pytest.mark.asyncio
+async def test_cleanup_db_data(client, monkeypatch):
 
-#     mongo_client = AsyncMongoMockClient()
-#     db = mongo_client["data"]
+    mongo_client = AsyncMongoMockClient()
+    db = mongo_client["data"]
 
-#     # add some sample data to the database
-#     test_data = {
-#         "metadata":
-#             {
-#                 "container_id": "1234",
-#                 "container_name": "test_container",
-#                 "container_image": "test_image"
-#             },
-#         "timestamp": "2023-03-20T12:00:00Z",
-#         "data":
-#             {
-#                 "cpu": 0.5,
-#                 "memory_perc": 50,
-#                 "memory_tot": 1024,
-#                 "total_rx": 100,
-#                 "total_tx": 100,
-#                 "io_read": 50,
-#                 "io_write": 50,
-#                 "healthcheck": True
-#             }
-#     }
-#     await db["test_collection"].insert_one(test_data)
+    monkeypatch.setattr(app_module.app, "db", mongo_client)
 
-#     # check that the data is in the database
-#     assert "data" in await mongo_client.list_database_names()
-#     assert await db["test_collection"].count_documents({}) > 0
+    # add some sample data to the database
+    test_data = {
+        "metadata":
+            {
+                "container_id": "1234",
+                "container_name": "test_container",
+                "container_image": "test_image"
+            },
+        "timestamp": "2023-03-20T12:00:00Z",
+        "data":
+            {
+                "cpu": 0.5,
+                "memory_perc": 50,
+                "memory_tot": 1024,
+                "total_rx": 100,
+                "total_tx": 100,
+                "io_read": 50,
+                "io_write": 50,
+                "healthcheck": True
+            }
+    }
+    await db["test_collection"].insert_one(test_data)
 
-#     #mock the mongoclient with test database
-#     monkeypatch.setattr(app_module.app, "db_data", db)
-#     response = await client.delete("/database/data")
-#     assert response.status_code == 200
-#     assert response.json() == {200: "Deleted DATA database"}
+    # check that the data is in the database
+    assert "data" in await mongo_client.list_database_names()
+    assert await db["test_collection"].count_documents({}) > 0
 
-#     # check that the DATA database is deleted
-#     assert "data" not in await mongo_client.list_database_names()
+    #mock the mongoclient with test database
+    monkeypatch.setattr(app_module.app, "db_data", db)
+    response = client.delete("/database/data")
+    assert response.status_code == 200
+    assert response.json() == {"200": "Deleted DATA database"}
 
-# @pytest.mark.asyncio
-# async def test_cleanup_db_client(client, monkeypatch):
+    # check that the DATA database is deleted
+    assert "data" not in await mongo_client.list_database_names()
 
-#     client = AsyncIOMotorClient(settings.MONGO_URI)
-#     db = client["users"]
 
-#     # add some sample data to the database
-#     test_data = {
-#         "username": "test_user",
-#         "email": "test@test.com",
-#         "hashed_password": "test_password",
-#         "servers": []
-#     }
-#     await db["user_data"].insert_one(test_data)
+@pytest.mark.asyncio
+async def test_cleanup_db_client(client, monkeypatch):
 
-#     # check that the data is in the database
-#     assert "users" in await client.list_database_names()
-#     assert await db["user_data"].count_documents({}) > 0
+    mongo_client = AsyncMongoMockClient()
+    db = mongo_client["users"]
 
-#     #mock the mongoclient with test database
-#     async def mock_get_mongo_client(request: Request):
-#         return db
+    monkeypatch.setattr(app_module.app, "db", mongo_client)
 
-#     monkeypatch.setattr(app_module.app, "get_db_users", mock_get_mongo_client)
+    # add some sample data to the database
+    test_data = {
+        "username": "test_user",
+        "email": "test@test.com",
+        "hashed_password": "test_password",
+        "servers": []
+    }
+    await db["user_data"].insert_one(test_data)
 
-#     response = await client.delete("/database/users")
-#     assert response.status_code == 200
-#     assert response.json() == {200: "Deleted CLIENT database"}
+    # check that the data is in the database
+    assert "users" in await mongo_client.list_database_names()
+    assert await db["user_data"].count_documents({}) > 0
 
-#     # check that the CLIENT database is deleted
-#     assert "users" not in await client.list_database_names()
+    monkeypatch.setattr(app_module.app, "db_users", db)
+
+    response = client.delete("/database/users")
+    assert response.status_code == 200
+    assert response.json() == {"200": "Deleted USERS database"}
+
+    # check that the CLIENT database is deleted
+    assert "users" not in await mongo_client.list_database_names()
