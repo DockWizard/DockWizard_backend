8f150cf3145784d5a27151bc3e340c4955259d59
Sagensagen <fsa028@uit.no>
2023-04-24 15:16:47 +0200

    :rocket: update CORS for production

--- backend/api/app.py
+++ backend/api/app.py
@@ -8,11 +8,9 @@ from fastapi.routing import APIRoute
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080", "http://localhost:3000",
+    "https://dockwizard.cloud"
 ]
 app.add_middleware(
     CORSMiddleware,
