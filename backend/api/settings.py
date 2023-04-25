ac6a92e0e52c34e4a712fc182d5ae17249865d77
Sagensagen <fsa028@uit.no>
2023-04-25 16:59:53 +0200

    testing prod env

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -1,8 +1,16 @@
+import os
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb://mongodb:27017/"
+    URI = "mongodb://mongodb:27017/"
+
+    if os.getenv("NODE_ENV") == "production":
+        MONGO_ROOT_PASSWORD = os.getenv("MONGO_ROOT_PASSWORD")
+
+        URI = f"mongodb://root:{MONGO_ROOT_PASSWORD}@mongodb:27017/"
+
+    MONGO_URI: str = URI
 
 
 settings = Settings()
