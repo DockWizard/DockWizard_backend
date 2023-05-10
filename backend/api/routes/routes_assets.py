5f2fc3f7d650ba3365d96d61a2fa70ff9cd87c90
Sagensagen <fsa028@uit.no>
2023-05-10 10:08:42 +0200

    Add agent binary directly

--- backend/api/routes/routes_assets.py
+++ backend/api/routes/routes_assets.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 @router.get("/agent")
 async def get_agent_binary(request: Request):
-    file_path = "assets/agent"
+    file_path = "assets/dockwizard-agent"
     return FileResponse(
         path=file_path,
         filename=file_path,
